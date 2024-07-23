8232456526d9bb1067de7a5a7d446566affe3028
nishit-95 <nishitvataliya003>
2024-07-23 16:08:06 +0530

    Add Login Page

--- CareerCatalyst/Controllers/UserController.cs
+++ CareerCatalyst/Controllers/UserController.cs
@@ -23,13 +23,13 @@ namespace CareerCatalyst.Controllers
         public ActionResult NewUser(UserMV UserMV)
         {
             if (ModelState.IsValid) { 
-                var checkuser = Db.UserTables.Where(u=>u.EmailAddress == UserMV.EmailAddress);
+                var checkuser = Db.UserTables.Where(u=>u.EmailAddress == UserMV.EmailAddress).FirstOrDefault();
                 if (checkuser != null) {
                     ModelState.AddModelError("EmailAddress", "Email is already Registered");
                     return View(UserMV);
                 }
 
-                checkuser = Db.UserTables.Where(u => u.UserName == UserMV.UserName);
+                checkuser = Db.UserTables.Where(u => u.UserName == UserMV.UserName).FirstOrDefault();
                 if (checkuser != null)
                 {
                     ModelState.AddModelError("UserName", "UserName is already Registered");
@@ -85,7 +85,7 @@ namespace CareerCatalyst.Controllers
                             company.CompanyName = UserMV.Company.CompanyName;
                             company.ContactNo = UserMV.ContactNo;
                             company.PhoneNo = UserMV.Company.PhoneNo;
-                            company.Logo = "~/Content/assets/img/logo.logo.png";
+                            company.Logo = "~/Content/assets/img/logo.png";
                             company.Description = UserMV.Company.Description;
                             Db.CompanyTables.Add(company);
                             Db.SaveChanges();
@@ -102,6 +102,40 @@ namespace CareerCatalyst.Controllers
                 }
             }
             return View(UserMV);
+        }
+        public ActionResult Login()
+        {
+            return View(new UserLoginMV());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLoginMV userLoginMV)
+        {
+            if (ModelState.IsValid) { 
+                var user = Db.UserTables.Where(u=>u.UserName == userLoginMV.UserName && u.password == userLoginMV.Password).FirstOrDefault();
+                if (user == null) {
+                    ModelState.AddModelError(string.Empty, "UserName and Paswword is Incorrect");
+                    return View(userLoginMV);
+                }
+                Session["UserID"] = user.UserID;
+                Session["UserName"] = user.UserName;
+                Session["UserTypeID"] = user.UserTypeID;
+
+                if (user.UserTypeID == 2) {
+                    Session["CompanyID"] = user.CompanyTables.FirstOrDefault().CompanyID;
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(userLoginMV);
+        }
+        public ActionResult Logout()
+        {
+            Session["UserID"] = string.Empty;
+            Session["UserName"] = string.Empty;
+            Session["CompanyID"] = string.Empty;
+            Session["UserTypeID"] = string.Empty;
+            return RedirectToAction("Index","Home");
         }
     }
 }
---
+++ CareerCatalyst/Models/UserLoginMV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CareerCatalyst.Models
+{
+    public class UserLoginMV
+    {
+        [Required(ErrorMessage="Required*")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Required*")]
+        public string Password { get; set; }
+    }
+}
